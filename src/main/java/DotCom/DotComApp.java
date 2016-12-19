58b819d46121f321b452868337e9e51ac4a3dff5
Tony Jones <xm39@global.tesco.org>
2016-12-19 14:44:07 +0000

    Completed the DotCom game improved Main.

--- src/main/java/DotCom/DotComApp.java
+++ src/main/java/DotCom/DotComApp.java
@@ -6,16 +6,29 @@ import java.util.ArrayList;
 
 public class DotComApp {
 
+    // Variables.
+    private static final int lengthOfEachDotCOm = 3;
+    private static final int maximumOverlapAttemptsPermitted = 3;
+    private static int numberOfDotComsRequired = 2;
+    private static int numberOfGuesses = 0;
+    private static int numberOfOverlapsEncountered = 0;
+    private static ArrayList<DotCom> dotComs = new ArrayList<DotCom>();
+
     public static void main(String[] args) {
+    /**********************************************************************
+     * The DotCom game !
+     **********************************************************************/
 
-        // Variables.
-        int numberOfDotComsRequired = 3;
-        int numberOfGuesses = 0;
-        int lengthOfDotCom = 3;
-        int numberOfOverlaps = 0;
-        int maximumOverlapAttempts = 3;
+        create_targets_for_game();
+        display_targets();
+        play_the_game();
 
-        ArrayList<DotCom> dotComs = new ArrayList<DotCom>();
+    }
+
+    private static void create_targets_for_game() {
+    /**********************************************************************
+     * create the DotCom targets in an array.
+     **********************************************************************/
 
         // Build the required number of DotComs.
         System.out.println("Creating " + numberOfDotComsRequired + " target(s).....");
@@ -32,8 +45,8 @@ public class DotComApp {
             for (DotCom dc : dotComs) {
                 if (dc != null) {
                     //System.out.println("To check " + dc.getAllLocationCells());
-                    for ( int i=0 ; i < lengthOfDotCom ; i++) {
-                        for ( int j=0 ; j < lengthOfDotCom ; j++) {
+                    for (int i = 0; i < lengthOfEachDotCOm; i++) {
+                        for (int j = 0; j < lengthOfEachDotCOm; j++) {
                             //System.out.println(d.getLocationCell(i) + " vs " + dc.getLocationCell(j));
                             if ( d.getLocationCell(i).equals(dc.getLocationCell(j))) {
                                 overlappingDotCom = true;
@@ -48,24 +61,34 @@ public class DotComApp {
                 // Add this new DotCom to the list.
                 dotComs.add(d);
                 x++;
-                numberOfOverlaps = 0;
+                numberOfOverlapsEncountered = 0;
             } else {
                 System.out.println("overlapping DotCom found");
-                numberOfOverlaps++;
-                if ( numberOfOverlaps >= maximumOverlapAttempts ) {
+                numberOfOverlapsEncountered++;
+                if ( numberOfOverlapsEncountered >= maximumOverlapAttemptsPermitted) {
                     System.out.println("Curtailing the target generation at " + x + " target(s) due to overlap.");
                     numberOfDotComsRequired = x;
                     break;
                 }
             }
-
         }
+    }
+
+    private static void display_targets(){
+    /**********************************************************************
+     * display the DotCom targets in an array.
+     **********************************************************************/
 
         // Display all the DotComs and their location.
         for (DotCom dc : dotComs) {
             System.out.println(dc.toString());
-            //System.out.println(dc.getAllLocationCells());
         }
+    }
+
+    private static void play_the_game() {
+    /**********************************************************************
+     * Play the game until the targets are destroyed.
+     **********************************************************************/
 
         // Run the game until there are no more targets to hit.
         guess:
@@ -78,8 +101,9 @@ public class DotComApp {
             // Check your shot against each of the DotComs.
             // If not a "miss", then it must be a hit - skip remaining DotComs.
             for (DotCom dc : dotComs) {
-                if ( ! dc.checkYourself(guessInput).equals("Miss") ) {
-                    break; }
+                if ( ! dc.getLocationCells().isEmpty() ) {
+                   if ( ! dc.checkYourself(guessInput).equals("Miss") ) { break; }
+                }
             }
 
             // Loop while there are DotComs to be hit.
@@ -88,9 +112,10 @@ public class DotComApp {
             }
 
             // Since we're here, we must have hit them all.
-            System.out.println("CONGRATULATIONS - you have hit all " + numberOfDotComsRequired + " target(s) in " + numberOfGuesses + " attempts.");
+            System.out.println("CONGRATULATIONS - you have hit all " +
+                                numberOfDotComsRequired + " target(s) in " +
+                                numberOfGuesses + " attempts.");
             break;
-
         }
     }
 }
