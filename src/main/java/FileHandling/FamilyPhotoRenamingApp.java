9dd7f5e7bd143776393c1f98f88a463ac64cbda9
jonesal26gc <tony.jones26gc@hotmail.co.uk>
2016-12-31 15:51:22 +0000

    minor change

--- src/main/java/FileHandling/FamilyPhotoRenamingApp.java
+++ src/main/java/FileHandling/FamilyPhotoRenamingApp.java
@@ -4,59 +4,73 @@ import java.io.File;
 
 public class FamilyPhotoRenamingApp {
 
+    private static final String NEW_LINE = "\n";
+
     public static void main(String[] args) {
         /*********************************************************************
          * This program loops through the photo album and renames the files
          * more appropriately.
          */
 
-        // Loop through the sub-foldrs of the parent folder.
+        // Loop through the sub-folders of the parent folder.
         File parentFolder = new File("d:/Family Photo Library");
-        File [] listOfSubFolders = parentFolder.listFiles();
+        if (parentFolder.isDirectory()) {
 
-        for ( File i : listOfSubFolders ){
+            // Iterate through the sub-folders of this parent directory.
+            File[] listOfSubFolders = parentFolder.listFiles();
+            for (File i : listOfSubFolders) {
 
-            // Process each sub-folder.
-            if ( i.isDirectory() ) {
+                // Process each sub-folder.
+                if (i.isDirectory()) {
 
-                // display the sub-folder name.
-                System.out.println("Sub-folder: " + i.getPath());
+                    //processSubFolder(i.getPath());
 
-                // Get the files within the sub-folder.
-                if ( i.isDirectory() ) {
+                    // display the sub-folder name.
+                    System.out.println(NEW_LINE + "Sub-folder: " + i.getPath());
 
+                    // Iterate through the files in the sub-folder.
                     File subFolder = new File(i.getPath());
                     File[] listOfFiles = subFolder.listFiles();
                     int fileNumber = 0;
                     for (File j : listOfFiles) {
-                        if (j.isFile()){
-                            if (! j.getName().startsWith("Thumb")) {
+                        if (j.isFile()) {
+                            if (!j.getName().startsWith("Thumb")) {
 
                                 // Increment the file counter.
                                 fileNumber++;
 
                                 // display the original filename.
-                                System.out.println("\nFile " + String.format("%04d",fileNumber) + ": " + j.getName());
+                                //System.out.println("File " + String.format("%04d",fileNumber) + ": " + j.getName());
 
                                 // Format the replacement filename.
                                 String newFilenameString = i.getPath() + "\\" +
-                                        j.getName().replace(j.getName().substring(0,j.getName().indexOf('.')),i.getName() + " - " + String.format("%04d",fileNumber));
+                                        j.getName().replace(j.getName().substring(0, j.getName().indexOf('.')), i.getName() +
+                                                " " + String.format("%04d", fileNumber));
 
+                                // Rename the file.
                                 File newFilename = new File(newFilenameString);
                                 System.out.println("New filename is: " + newFilename.getName());
-                                j.renameTo(newFilename);
+                                //j.renameTo(newFilename);
+
                             } else {
-                                System.out.println("Deleting file: " + j.getName());
-                                j.delete();
+                                System.out.println("inappropriate file - Deleting: " + j.getName());
+                                //j.delete();
                             }
+                        } else {
+                            System.out.println("misplaced directory - expected file(s) only - skipping " + j.getName());
                         }
                     }
                 } else {
                     System.out.println("misplaced file - expected sub-folder - skipping " + i.getName());
                 }
-            }
 
+            }
+        } else {
+            System.out.println("Parent library is not actually a folder.");
         }
+    }
+
+    private static void processSubFolder() {
     }
 
     public static File newFile(File oldFile){
